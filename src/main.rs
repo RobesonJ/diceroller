03099586de9638635615add234b68135ddbe970a
RobesonJ <robeson943@gmail.com>
2024-08-18 22:05:10 -0400

    tokenize

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,129 @@
 
 use std::env;
 use rand::Rng;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();   
-    
+    let args: Vec<String> = env::args().collect();
+    let roll = "roll".to_string();   
     for i in 0..args.len(){
-        match args[i].to_lowercase().as_str(){            
-            "roll" => make_die(&args[i+1]),
-            &_ => continue,
+        if args[i] == roll {
+            parsing(tokenize(&args[i+1]))
         }
-        break;
+    }
+    
+   
+}
+fn parsing(tokens: Vec<Token>){
+    for token in tokens{
+        println!("Token: {}", token.lexeme)
     }
 }
+enum TokenKind {
+    // Single-character tokens.
+    LEFT_PAREN, 
+    RIGHT_PAREN, 
+    LEFT_BRACE, 
+    RIGHT_BRACE,
+    COMMA, 
+    DOT, 
+    MINUS, 
+    PLUS, 
+    SEMICOLON, 
+    SLASH, 
+    STAR,
+    DIE, // d
+  
+    // One or two character tokens.
+    BANG, 
+    BANG_EQUAL,
+    EQUAL, 
+    EQUAL_EQUAL,
+    GREATER, 
+    GREATER_EQUAL,
+    LESS, 
+    LESS_EQUAL,
+  
+    // Literals.     
+    STRING, 
+    NUMBER,
+  
+    // Keywords.
+    ROLL, // roll
+  
+    
+  }
+struct Token{
+    kind: TokenKind,
+    lexeme: String,
+
+}
+impl Token {
+    fn new() -> Token {
+        Token{kind:TokenKind::STRING, lexeme:"".to_string()}
+    }
+    
+}
+struct Lexer {
+    source: Vec<String>,
+    tokens: Vec<Token>,
+}
+fn is_at_end(current: usize, source: String)-> bool{
+    current >= source.len()
+}
+
+fn tokenize(source: &String) -> Vec<Token> {
+    //I need a match statement to tie TokenKind to its prospective String value
+    let char_source: Vec<char> = source.chars().collect();
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut numstring = "".to_string();
+    let mut token = Token::new(); 
+    for mut i in 0 .. char_source.len(){        
+        while i < char_source.len() && char_source[i].is_numeric() {            
+                numstring.push_str(&char_source[i].to_string());                
+                i += 1;
+        }
+        if numstring != "".to_string(){
+            token = Token { kind: TokenKind::NUMBER, lexeme: numstring.clone() };
+            tokens.push(token);
+        }        
+        numstring = "".to_string();        
+        if i < char_source.len(){
+            match char_source[i] {
+                '+' => {token = Token { kind: TokenKind::PLUS, lexeme: "+".to_string() };},
+                '-' => {token = Token { kind: TokenKind::MINUS, lexeme: "-".to_string() };},
+                'd' => {                    
+                    token = Token { kind: TokenKind::DIE, lexeme: "d".to_string() };
+                } ,                          
+                _ => continue,
+            }        
+            tokens.push(token);
+            
+        }
+        
+    }
+    tokens
+    
+    
+}
+ 
 struct Die {    
     pip: i32,
     modifier: i32,
     sign: char,
+}
+impl Die {
+    fn roll(&self, count: i32){
+        for _i in 0 ..count{
+            let mut total = 0;        
+            let result: i32 = rand::thread_rng().gen_range(1..=self.pip);        
+            match self.sign {
+                '+' => {total = 1 * result + self.modifier;},
+                '-' => {total = 1 * result - self.modifier;},
+                _ => printerr("invalid sign"),
+            }
+            println!("{}", total);
+        }
+        
+    } 
 }
 fn make_die(arg: &str){
     let mut die = Die{        
@@ -46,21 +153,7 @@ fn make_die(arg: &str){
     
     
 }
-impl Die {
-    fn roll(&self, count: i32){
-        for _i in 0 ..count{
-            let mut total = 0;        
-            let result: i32 = rand::thread_rng().gen_range(1..=self.pip);        
-            match self.sign {
-                '+' => {total = 1 * result + self.modifier;},
-                '-' => {total = 1 * result - self.modifier;},
-                _ => printerr("invalid sign"),
-            }
-            println!("{}", total);
-        }
-        
-    } 
-}
+
  
 fn printerr(e: &str){
     println!("{}", e);
